527a3e925b2aa5a6ab1c04d155a9fca2036ff16f
sufailps <67498736+sufailps@users.noreply.github.com>
2021-09-21 11:47:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -13,4 +13,11 @@ print(f"Threads_connected => {r['Threads_connected']}")
 print(f"Threads_running => {r['Threads_running']}")
 print(f"Queries => {r['Queries']}")
 print(f"Max_used_connections => {r['Max_used_connections']}")
+print("Process List")
+cur.execute("select * from PROCESSLIST")
+res1=cur.fetchall()
+#print(res1)
+for i in res1:
+	print(f"Id : {i[0]} | User info : {i[1]} | Host : {i[2]} | DB : {i[3]} | Command : {i[4]} | Time : {i[5]} | State : {i[6]} | Info : {i[7]}")
+
 cur.close()
